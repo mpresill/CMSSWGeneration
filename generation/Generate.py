a95165514c4f9e194689a8404b896ec40573aa9b
IreneZoi <irene.zoi@cern.ch>
2023-02-01 01:31:17 +0100

    implemented singularity job splitting, under test!

--- generation/Generate.py
+++ generation/Generate.py
@@ -175,9 +175,6 @@ def generate(name, year, gridpack, removeOldRoot, dipoleRecoil, events, jobs, do
                 wrapper += 'tar -xzvf {}.tgz\n'.format(Steps[year][k]['release'])
                 wrapper += 'rm {}.tgz\n'.format(Steps[year][k]['release'])
                 wrapper += 'cd {}/src/\n'.format(Steps[year][k]['release'])
-                #if 'slc6' in Steps[year][k]['SCRAM_ARCH']: 
-                #    print " ADDING SINGULARITY!!!!!"
-                #    wrapper += "cmssw-slc6-condor \n"
                 wrapper += 'export SCRAM_ARCH={}\n'.format(Steps[year][k]['SCRAM_ARCH'])
                 wrapper += 'scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile\n'
                 wrapper += 'eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers\n'
@@ -219,9 +216,209 @@ def generate(name, year, gridpack, removeOldRoot, dipoleRecoil, events, jobs, do
         if (doBatch==1):
             process = subprocess.Popen('cd output/{}; condor_submit submit.jdl; cd -'.format(name), shell=True)
             process.wait()
-    else:
-        print "FIXMEEEEEEEEEEE"
-        sys.exit()
+    else: # below implementation for different os
+        # config for lhe, premix and miniAOD with slc6
+        fileToTransfer = [os.path.abspath(gridpack)]
+        inputsCfg = glob.glob("data/input_{}/*.py".format(year))
+        inputsCfg = list(map(lambda k: os.path.abspath(k), inputsCfg))
+        inputsCfg = [ x for x in inputsCfg if "Nano" not in x ]
+        fileToTransfer.extend(inputsCfg)
+        print " inputsCfg ",inputsCfg
+        print " year ",year
+        print glob.glob("data/input_{}/*Mini*.py".format(year))
+        outputFile = glob.glob("data/input_{}/*Mini*.py".format(year))[0].split("/")[-1].split("_1_")[0]
+        for cmssw in cmssws[:-1]:
+            fileToTransfer.append(os.path.abspath(glob.glob("data/CMSSWs/{}.tgz".format(cmssw))[0]))
+
+
+        jdl_slc6 = "Universe = vanilla \n"
+        if 'slc6' in Steps[year]['lhe']['SCRAM_ARCH']:
+             jdl_slc6 += '+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel6" \n'
+             jdl_slc6 += 'Requirements = HasSingularity \n'
+        jdl_slc6 += "Executable = wrapper.sh\n"
+        jdl_slc6 += "request_cpus = 8 \n"
+        jdl_slc6 += "should_transfer_files = YES\n"
+        jdl_slc6 += "Error = log/$(proc).err_$(Step)\n"
+        jdl_slc6 += "Output = log/$(proc).out_$(Step)\n"
+        jdl_slc6 += "Log = log/$(proc).log\n"
+        jdl_slc6 += "Proxy_filename = x509up\n"
+        username = getpass.getuser()
+        jdl_slc6 += "Proxy_path = /afs/cern.ch/user/{}/{}/private/$(Proxy_filename)\n".format(username[0],username)
+        jdl_slc6 += "arguments = $(Proxy_path) $(proc) {}\n".format(events)
+        jdl_slc6 += "transfer_input_files = $(Proxy_path), {}\n".format(", ".join(fileToTransfer))
+        jdl_slc6 += 'transfer_output_remaps = "{}.root = {}/$(proc)_$(Cluster)_$(Step).root"\n'.format(outputFile, os.path.abspath("output/{}/root".format(name)))
+        jdl_slc6 += "when_to_transfer_output = ON_EXIT\n"
+        jdl_slc6 += "+JobFlavour = \"tomorrow\"\n"
+        jdl_slc6 += "Queue {} proc in ({})\n".format(jobs, name)
+
+        with open("output/{}/submit_slc6.jdl".format(name), "w") as file:
+            file.write(jdl_slc6)
+
+
+        wrapper_slc6 =  "#!/bin/bash\n"
+        wrapper_slc6 += 'echo "Starting job on " `date` #Date/time of start of job\n'
+        wrapper_slc6 += 'echo "Running on: `uname -a`" #Condor job is running on this node\n'
+        wrapper_slc6 += 'echo "System software: `cat /etc/redhat-release`" #Operating System on that node\n'
+        wrapper_slc6 += 'source /cvmfs/cms.cern.ch/cmsset_default.sh\n'    
+        wrapper_slc6 += 'export X509_USER_PROXY=$1\n'
+        wrapper_slc6 += 'voms-proxy-info -all\n'
+        wrapper_slc6 += 'voms-proxy-info -all -file $1\n'
+
+        openCMSSW = ""
+
+        donePremixFirst = False
+        totalSteps.insert(1, "premix")
+        totalSteps_slc6 = totalSteps[:-1]
+        #print " --------------- commented premix!!!!! ------------------ "
+        filesToRemove = [gridpack.split("/")[-1]]
+        for k in totalSteps_slc6:
+            wrapper_slc6 += "#Working on {} step\n\n".format(k)
+            file = list(filter(lambda j: k.lower() in j.lower(), inputsCfg))[0].split("/")[-1]
+            if k == "premix":
+                if not donePremixFirst:
+                    if "_2_" in file:
+                        # should first run premix _1_
+                        file = file[:-8]+"1_cfg.py"
+                    donePremixFirst = True
+                else:
+                    if "_1_" in file:
+                        # now should run premix _2_
+                        file = file[:-8]+"2_cfg.py"
+
+            if Steps[year][k]['release'] != openCMSSW:
+                if openCMSSW != "":
+                    wrapper_slc6 += "rm -rf {} \n".format(openCMSSW)
+                wrapper_slc6 += 'echo "Opening {}"\n'.format(Steps[year][k]['release'])
+                wrapper_slc6 += 'tar -xzvf {}.tgz\n'.format(Steps[year][k]['release'])
+                wrapper_slc6 += 'rm {}.tgz\n'.format(Steps[year][k]['release'])
+                wrapper_slc6 += 'cd {}/src/\n'.format(Steps[year][k]['release'])
+                #if 'slc6' in Steps[year][k]['SCRAM_ARCH']: 
+                #    print " ADDING SINGULARITY!!!!!"
+                #    wrapper += "cmssw-slc6-condor \n"
+                wrapper_slc6 += 'export SCRAM_ARCH={}\n'.format(Steps[year][k]['SCRAM_ARCH'])
+                wrapper_slc6 += 'scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile\n'
+                wrapper_slc6 += 'eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers\n'
+                wrapper_slc6 += 'echo $CMSSW_BASE "is the CMSSW we have on the local worker node"\n'
+                wrapper_slc6 += 'cd ../../\n'
+                openCMSSW = Steps[year][k]['release']
+            print " -------------     DOING SUBSTITUTIONS!!!!!!!!"    
+            if k == 'lhe':
+                print " **** lhe time!!! ",gridpack," ",file
+                #wrapper += "sed -i 's#^.*tarball.tar.xz.*$#    args = cms.vstring(\"./{}\"),#' -i {}\n".format(gridpack.split("/")[-1], file)
+                wrapper_slc6 += "sed -i 's#^.*tarball.tar.xz.*$#    args = cms.vstring(\"{}\"),#' -i {}\n".format(gridpack, file)
+                #wrapper += 'sed -i "s/^.*input = .*$/    input = cms.untracked.uint32({})/g" -i {}\n'.format(events, file)
+                #wrapper += 'sed -i "s/nevts:*$/nevts:{}\'),/g" -i {}\n'.format(events, file)
+                #wrapper += 'sed -i "s/^.*nEvents = .*$/    nEvents = cms.untracked.uint32({}),/g" -i {}\n'.format(events, file)
+                # if dipoleRecoil:
+                #     wrapper += "sed '/^.*pythia8CP5Settings[^=]*=.*/i \ \ \ \ \ \ \ \ processParameters = cms.vstring(\"SpaceShower:dipoleRecoil = on\"),' {} -i\n".format(file)
+            wrapper_slc6 += "date\n"
+            wrapper_slc6 += "cmsRun {}\n".format(file)
+            if removeOldRoot:
+                if k == "lhe":
+                    filesToRemove.append(file.split("_")[0]+".root")
+                    filesToRemove.append(file.split("_")[0]+"_inLHE.root")
+
+                elif k == "premix" and "_1_" not in file:
+                    filesToRemove.append(file.split("_")[0]+".root")
+                    filesToRemove.append(file.split("_")[0]+"_0.root")
+                # elif k == "miniAOD": # now we want to keep the miniAOD!!!!
+                #     filesToRemove.append(file.split("_")[0]+".root")
+            wrapper_slc6 += "\n\n"
+        wrapper_slc6 += "rm {}\n".format(" ".join(filesToRemove))
+        wrapper_slc6 += "rm -rf {} *py\n".format(openCMSSW)
+        wrapper_slc6 += "date\n"
+
+        with open("output/{}/wrapper_slc6.sh".format(name), "w") as file:
+            file.write(wrapper_slc6)
+
+        process = subprocess.Popen("chmod +x output/{}/wrapper_slc6.sh".format(name), shell=True)
+        process.wait()
+        if (doBatch==1):
+            process = subprocess.Popen('cd output/{}; condor_submit submit_slc6.jdl; cd -'.format(name), shell=True)
+            process.wait()
+            
+        # config for nanoAOD with slc7
+        miniAOD = os.path.abspath('output/{}/root/{}.root'.format(name,outputFile))
+        fileToTransfer = [miniAOD]
+        inputsCfg = glob.glob("data/input_{}/*.py".format(year))
+        inputsCfg = list(map(lambda k: os.path.abspath(k), inputsCfg))
+        inputsCfg = inputsCfg[-1]
+        fileToTransfer.append(inputsCfg)
+        print " inputsCfg ",inputsCfg
+        print " year ",year
+        print glob.glob("data/input_{}/*Nano*.py".format(year))
+        outputFile = glob.glob("data/input_{}/*Nano*.py".format(year))[0].split("/")[-1].split("_1_")[0]
+        fileToTransfer.append(os.path.abspath(glob.glob("data/CMSSWs/{}.tgz".format(cmssws[-1]))[0]))
+
+
+        jdl = "Universe = vanilla \n"
+        jdl += "Executable = wrapper.sh\n"
+        jdl += "request_cpus = 8 \n"
+        jdl += "should_transfer_files = YES\n"
+        jdl += "Error = log/$(proc).err_$(Step)\n"
+        jdl += "Output = log/$(proc).out_$(Step)\n"
+        jdl += "Log = log/$(proc).log\n"
+        jdl += "Proxy_filename = x509up\n"
+        username = getpass.getuser()
+        jdl += "Proxy_path = /afs/cern.ch/user/{}/{}/private/$(Proxy_filename)\n".format(username[0],username)
+        jdl += "arguments = $(Proxy_path) $(proc) {}\n".format(events)
+        jdl += "transfer_input_files = $(Proxy_path), {}\n".format(", ".join(fileToTransfer))
+        jdl += 'transfer_output_remaps = "{}.root = {}/$(proc)_$(Cluster)_$(Step).root"\n'.format(outputFile, os.path.abspath("output/{}/root".format(name)))
+        jdl += "when_to_transfer_output = ON_EXIT\n"
+        jdl += "+JobFlavour = \"tomorrow\"\n"
+        jdl += "Queue {} proc in ({})\n".format(jobs, name)
+
+        with open("output/{}/submit.jdl".format(name), "w") as file:
+            file.write(jdl)
+
+
+        wrapper =  "#!/bin/bash\n"
+        wrapper += 'echo "Starting job on " `date` #Date/time of start of job\n'
+        wrapper += 'echo "Running on: `uname -a`" #Condor job is running on this node\n'
+        wrapper += 'echo "System software: `cat /etc/redhat-release`" #Operating System on that node\n'
+        wrapper += 'source /cvmfs/cms.cern.ch/cmsset_default.sh\n'    
+        wrapper += 'export X509_USER_PROXY=$1\n'
+        wrapper += 'voms-proxy-info -all\n'
+        wrapper += 'voms-proxy-info -all -file $1\n'
+
+        openCMSSW = ""
+
+        donePremixFirst = False
+        k = totalSteps[-1]
+        #print " --------------- commented premix!!!!! ------------------ "
+        filesToRemove = [miniAOD.split("/")[-1]]
+        wrapper += "#Working on {} step\n\n".format(k)
+        file = inputsCfg.split("/")[-1]
+        if Steps[year][k]['release'] != openCMSSW:
+            if openCMSSW != "":
+                wrapper += "rm -rf {} \n".format(openCMSSW)
+            wrapper += 'echo "Opening {}"\n'.format(Steps[year][k]['release'])
+            wrapper += 'tar -xzvf {}.tgz\n'.format(Steps[year][k]['release'])
+            wrapper += 'rm {}.tgz\n'.format(Steps[year][k]['release'])
+            wrapper += 'cd {}/src/\n'.format(Steps[year][k]['release'])
+            #if 'slc6' in Steps[year][k]['SCRAM_ARCH']: 
+            #    print " ADDING SINGULARITY!!!!!"
+            #    wrapper += "cmssw-slc6-condor \n"
+            wrapper += 'export SCRAM_ARCH={}\n'.format(Steps[year][k]['SCRAM_ARCH'])
+            wrapper += 'scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile\n'
+            wrapper += 'eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers\n'
+            wrapper += 'echo $CMSSW_BASE "is the CMSSW we have on the local worker node"\n'
+            wrapper += 'cd ../../\n'
+            openCMSSW = Steps[year][k]['release']
+
+        wrapper += "date\n"
+        wrapper += "cmsRun {}\n".format(file)
+        wrapper += "\n\n"
+        wrapper += "rm {}\n".format(" ".join(filesToRemove))
+        wrapper += "rm -rf {} *py\n".format(openCMSSW)
+        wrapper += "date\n"
+
+        with open("output/{}/wrapper.sh".format(name), "w") as file:
+            file.write(wrapper)
+
+        process = subprocess.Popen("chmod +x output/{}/wrapper.sh".format(name), shell=True)
+        process.wait()
+        #this step should be submitted only when the previous is done, so batch submission option is removed here
 
 def helperJsonParse(Samples, sample):
     params = []
