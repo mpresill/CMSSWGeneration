6f46f01247f736426dcff4787c23652570bd7e65
mpresill <mpresill@portal1-centos7.etp.kit.edu>
2024-05-23 13:14:41 +0200

    adding fixes

--- generation/scripts/2018_official/wrapper.sh
+++ generation/scripts/2018_official/wrapper.sh
@@ -3,25 +3,36 @@ echo "Starting job on " `date` #Date/time of start of job
 echo "Running on: `uname -a`" #Condor job is running on this node
 echo "System software: `cat /etc/redhat-release`" #Operating System on that node
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-infile={INPUT}
-echo ${infile}
-xrdcp -f root://cmsxrootd.fnal.gov/${infile} SMP-RunIIAutumn18MiniAOD-00272.root
 #Working on nanoAOD step
 
-echo "Opening CMSSW_10_2_22"
-tar -xzvf CMSSW_10_2_22.tgz
-rm CMSSW_10_2_22.tgz
-cd CMSSW_10_2_22/src/
-export SCRAM_ARCH=slc6_amd64_gcc700
+echo "Opening CMSSW_10_2_22_ZV_EFT"
+tar -xzvf CMSSW_10_2_22_ZV_EFT.tgz
+rm CMSSW_10_2_22_ZV_EFT.tgz
+cd CMSSW_10_2_22_ZV_EFT/src/
+#export SCRAM_ARCH=slc6_amd64_gcc700
 scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile
 eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
 echo $CMSSW_BASE "is the CMSSW we have on the local worker node"
 cd ../../
 date
+
 # sed -i "s|file:SMP-RunIIAutumn18MiniAOD-00272.root|file:SMP-RunIIAutumn18MiniAOD-00272_${2}.root|g" -i SMP-RunIIAutumn18NanoAODv7-00274_1_cfg.py
+# Impostare il percorso della proxy
+export X509_USER_PROXY=/home/mpresill/.globus/x509_proxy
+echo "Using proxy at: $X509_USER_PROXY"
+# Verifica che il file della proxy sia presente
+if [ ! -f "$X509_USER_PROXY" ]; then
+    echo "Proxy file not found!"
+    exit 1
+fi
+infile={INPUT}
+echo ${infile}
+xrdcp -f root://xrootd-cms.infn.it/${infile} SMP-RunIIAutumn18MiniAOD-00272.root
 cmsRun SMP-RunIIAutumn18NanoAODv7-00274_1_cfg.py
-xrdcp -f SMP-RunIIAutumn18NanoAODv7-00274.root root://cmseos.fnal.gov//store/group/lnujj/aQGC_VVJJ_PrivateNanoProduction_2018//2018_{SAMPLE}/NanoAODv7/SMP-RunIIAutumn18NanoAODv7-00274_{NUMBER}.root
+mkdir -p /ceph/mpresill/privateProduction_aQGC_VBS_semileptonic_nanov7/aQGC_VVJJ_PrivateNanoProduction_2018/2018_{SAMPLE}/NanoAODv7/ ##check if crashes if the folder exists
+cp -f SMP-RunIIAutumn18NanoAODv7-00274.root /ceph/mpresill/privateProduction_aQGC_VBS_semileptonic_nanov7/aQGC_VVJJ_PrivateNanoProduction_2018//2018_{SAMPLE}/NanoAODv7/SMP-RunIIAutumn18NanoAODv7-00274_{NUMBER}.root
+#xrdcp -f SMP-RunIIAutumn18NanoAODv7-00274.root /ceph/mpresill/privateProduction_aQGC_VBS_semileptonic_nanov7/aQGC_VVJJ_PrivateNanoProduction_2018//2018_{SAMPLE}/NanoAODv7/SMP-RunIIAutumn18NanoAODv7-00274_{NUMBER}.root
 
 rm SMP-RunIIAutumn18MiniAOD-00272_{NUMBER}.root SMP-RunIIAutumn18NanoAODv7-00274_1_cfg.py
-rm -rf CMSSW_10_2_22 
+rm -rf CMSSW_10_2_22_ZV_EFT 
 date
--- generation/scripts/submissionMaker.py
+++ generation/scripts/submissionMaker.py
@@ -1,24 +1,35 @@
 import os
 import fileinput
 
-year    = "2016"
+import subprocess
+import re
+import json
+import sys
+
+
+year    = "2018"
 samples = [
-"aQGC_WMhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WPlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_ZlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WPhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WPhadWMlepJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WMlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WPlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
-# "aQGC_WMlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
-"aQGC_WPlepWPhadJJ_EWK_LO_SM_mjj100_pTj10"
+#"aQGC_WMhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WPlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_ZlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WPhadZlepJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WPhadWMlepJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WMlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WPlepZhadJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WMlepWMhadJJ_EWK_LO_SM_mjj100_pTj10",
+#"aQGC_WPlepWPhadJJ_EWK_LO_SM_mjj100_pTj10"
+"test2",
 ]
 
 cwd = os.getcwd()
+print "we are here: ", cwd
 for sample in samples:
     print "sample: ",sample
-    filepath = '{}_TuneCP5_MINIAOD_{}.txt'.format(sample,year)
-    directory = '{}_{}_official/'.format(year,sample)
+    filepath = '/work/mpresill/nanov7_private_production/CMSSWGeneration/generation/output/{}_TuneCP5_MINIAOD_{}.txt'.format(sample,year)
+    print "filepath: ", filepath
+    directory = '{}_official/'.format(year)
+    print "directory: ", directory #2017_official
+    
     files = []
     cnt = 0
     with open(filepath) as fp:
@@ -30,29 +41,41 @@ for sample in samples:
            cnt += 1
     # os.system('cd {}/'.format(directory))
     # print ( cnt )
-    wrapperFile = 'wrapper.sh'
-    jdlFile     = 'submit.jdl'
-    os.mkdir(directory+'wrappers')
-    os.mkdir(directory+'submitters')
+    wrapperFile = '{}_official/wrapper.sh'.format(year)
+    jdlFile     = '{}_official/submit.jdl'.format(year)
+    #print "wrapperFile: ", wrapperFile # 2017_official/wrapper.sh
+    #print "jdlFile: ", jdlFile         # 2017_official/submit.jdl
+    #os.mkdir(directory+'wrappers')
+    #os.mkdir(directory+'submitters')
     for i in range(cnt):
-        wrapperFile_new = 'wrappers/{}'.format(wrapperFile.replace(".sh","_"+str(i)+".sh"))
-        jdlFile_new = '{}submitters/{}'.format(directory,jdlFile.replace(".jdl","_"+str(i)+".jdl"))
-        filein = open('{}{}'.format(directory,wrapperFile))
-        fileout = open(directory+wrapperFile_new,"wt")
+        #wrapperFile_new = '{}'.format(wrapperFile.replace(".sh","_"+str(i)+".sh"))
+        #jdlFile_new = '{}'.format(jdlFile.replace(".jdl","_"+str(i)+".jdl"))
+        wrapperFile_new = '{}'.format(wrapperFile.replace(".sh","_"+sample+"_"+str(i)+".sh"))
+        jdlFile_new = '{}'.format(jdlFile.replace(".jdl","_"+sample+"_"+str(i)+".jdl"))
+        filein = open('{}'.format(wrapperFile))
+        fileout = open(wrapperFile_new,"wt")
         for line in filein:
             fileout.write(line.replace('{INPUT}', files[i]).replace('{NUMBER}', str(i)).replace('{SAMPLE}',sample))
         filein.close()
         fileout.close()
 
-        filein = open('{}{}'.format(directory,jdlFile))
+        filein = open('{}'.format(jdlFile))
         fileout = open(jdlFile_new,"wt")
         for line in filein:
             fileout.write(line.replace('{WRAPPER}', wrapperFile_new).replace('{STEP}', str(i)).replace('{FILE}',year+"_"+sample))
         filein.close()
         fileout.close()
 
-    os.chdir(directory)
+#    os.chdir(directory)
     for i in range(cnt):
-        os.system("condor_submit submitters/submit_{}.jdl".format(str(i)))
-    
+        os.system("condor_submit {}submit_{}_{}.jdl".format(directory,sample,str(i)))
+        print "submitting...", "condor_submit {}submit_{}_{}.jdl".format(directory,sample,str(i))
+
+
+
+
     os.chdir(cwd)
+
+
+
+
